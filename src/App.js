fd29634468b345558d9d374ac5b9a39fc69e0748
PUMIN-L <puminbg@gmail.com>
2024-05-07 22:16:31 +0700

    savetest2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [a, setA] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={() => setA((prev) => prev + 1)}>
+        <code>PUMIN TEST 2 and a = {a}</code>
+        
         </p>
         <a
           className="App-link"
